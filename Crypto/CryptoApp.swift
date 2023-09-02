e2b3f77be59e231c51bdc2b6fc6b5b795c872a92
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2023-09-02 12:39:16 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Surya on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
